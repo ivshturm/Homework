f6f3b017f623b07d5312d906615d156a12de0ae5
Ivan Maruev <ivshturm@mail.ru>
2018-02-27 09:59:28 +0300

    Исправлены ошибки, пройдены основные тесты.

--- hw02/src/main/java/ru/sberbank/homework/checker/Tst.java
+++ hw02/src/main/java/ru/sberbank/homework/checker/Tst.java
@@ -1,5 +1,11 @@
 package ru.sberbank.homework.checker;
 
+import java.text.DecimalFormat;
+
 public class Tst {
+    public static void main(String[] args) {
+        long i = 0b1111111_11111111_11111111_11111111_11111111_11111111_11111111_11111111L + 2;
+        long a = Long.parseLong("111111111111111111111111111111111111111111111111111111111111111", 2);
+    }
 
 }
